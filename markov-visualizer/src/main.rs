8a488d3fc81fbbdb8a05b1bdfbd1618cc3f0882d
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-06-15 21:00:09 +0100

    feat: use clap to provide a CLI

--- markov-visualizer/src/main.rs
+++ markov-visualizer/src/main.rs
@@ -11,14 +11,42 @@ use markov_runner::{
     step_markov_1d,
 };
 
+use clap::Parser;
+
+/// A simple visualization tool for Markov algorithms
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The algorithm to run
+    #[clap(short, long, value_parser)]
+    algorithm: Algorithm,
+
+    /// The input for the algorithm
+    #[clap(short, long, value_parser)]
+    input: String,
+}
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+enum Algorithm {
+    BinaryToUnary,
+}
+
 fn main() -> Result<()> {
+    let args = Args::parse();
+
     let mut stdout = stdout();
-    let node = Sequence::new()
+    let node = match args.algorithm {
+        Algorithm::BinaryToUnary => binary_to_unary_node(),
+    };
+    print_steps(&mut stdout, &args.input, &node, Duration::from_millis(500))?;
+    Ok(())
+}
+
+fn binary_to_unary_node() -> Sequence {
+    Sequence::new()
         .add_node(Rule::new("1", "0x"))
         .add_node(Rule::new("x0", "0xx"))
-        .add_node(Rule::new("0", ""));
-    print_steps(&mut stdout, "101", &node, Duration::from_millis(500))?;
-    Ok(())
+        .add_node(Rule::new("0", ""))
 }
 
 fn print_step(stdout: &mut Stdout, r: &str) -> Result<()> {
